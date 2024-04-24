794376009aaee7d70683d0bea295596b66e971c4
Luis Silva <luis.silva1mon@gmail.com>
2024-04-24 10:38:51 -0600

    Save to pc

--- classes/User.php
+++ classes/User.php
@@ -1,11 +1,38 @@
 <?php
-
 class User {
-    private $username;
-    private $mail;
-    public function __construct() {
-        
+    private $_id;
+    private $_username;
+    private $_mail;
+    private $_password_hash;
+    private $_salt;
+    private $_created_at;
+
+
+    public function __construct($username, $mail) {
+        $this->_username = $username;
+        $this->_mail = $mail;
+    }
+    
+    public function getUsername() {
+        return $this->_username;
+    }
+
+    public function setUsername($username) {
+        $this->_username = $username;
+    }
+
+    public function getMail() {
+        return $this->_mail;
+    }
+
+    public function setMail($mail) {
+        $this->_mail = $mail;
+    }
+    
+    public function toArray(){
+        return [
+            'username' => $this->_username,
+            'mail' => $this->_mail
+        ];
     }
 }
-
-?>
