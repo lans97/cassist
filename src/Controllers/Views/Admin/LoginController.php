694c65a685e85f1a786ea9331cec3cb2686aa06e
Luis Silva <luis.silva1mon@gmail.com>
2024-04-25 12:18:43 -0600

    send help

--- src/Controllers/Views/Admin/LoginController.php
+++ src/Controllers/Views/Admin/LoginController.php
@@ -3,7 +3,7 @@
 class LoginController {
     public function index() {
         include PROJECT_ROOT . "templates/header.php";
-        include PROJECT_ROOT . "views/admin/login.php";
+        include PROJECT_ROOT . "Views/Admin/login.php";
         include PROJECT_ROOT . "templates/footer.php";
     }
 }
