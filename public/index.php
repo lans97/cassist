f87bb25429bc3486018aa49c801c4747771c3cb3
Luis Silva <luis.silva1mon@gmail.com>
2024-04-25 11:36:02 -0600

    playing with route parsing schemes

--- public/index.php
+++ public/index.php
@@ -6,8 +6,9 @@ define('PROJECT_ROOT', __DIR__ . '/../');
 
 $basePath = '/';
 $requestedUrl = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$route = explode('/', $requestedUrl);
 
-var_dump($requestedUrl);
+var_dump($route);
 
 if (strpos($requestedUrl, '/api') === 0) {
     include PROJECT_ROOT . 'src/routes/apiRoutes.php';
