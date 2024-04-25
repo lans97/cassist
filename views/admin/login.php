bf2e5545f0f02757430c3b8131d69815082866e1
Luis Silva <luis.silva1mon@gmail.com>
2024-04-25 14:43:55 -0600

    Nice login page

--- views/admin/login.php
+++ views/admin/login.php
@@ -1,1 +1,18 @@
-<h1>Login</h1>
+<div class="container d-flex justify-content-center align-items-center vh-100">
+    <div class="card">
+        <div class="card-body">
+            <h2 class="card-title">Login</h2>
+            <form id="loginForm" action="login.php" method="POST">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" class="form-control" required>
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Login</button>
+            </form>
+        </div>
+    </div>
+</div>
