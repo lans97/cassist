3ce35fc74956912ec68a0ab2748f92560fb3c84b
Luis Silva <luis.silva1mon@gmail.com>
2024-04-25 11:02:25 -0600

    pls help im din

--- public/index.php
+++ public/index.php
@@ -9,9 +9,9 @@ $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 var_dump($requestUri);
 
 if (strpos($requestUri, '/api') === 0) {
-    require_once PROJECT_ROOT . 'src/routes/apiRoutes.php';
+    include PROJECT_ROOT . 'src/routes/apiRoutes.php';
 } elseif (strpos($requestUri, '/admin') === 0) {
-    require_once PROJECT_ROOT . 'src/routes/adminRoutes.php';
+    include PROJECT_ROOT . 'src/routes/adminRoutes.php';
 } else {
-    require_once PROJECT_ROOT . 'src/routes/webRoutes.php';
+    include PROJECT_ROOT . 'src/routes/webRoutes.php';
 }
--- src/routes/adminRoutes.php
+++ src/routes/adminRoutes.php
@@ -1,5 +1,7 @@
 <?php
 
+echo "hi"
+
 switch ($requestUri) {
     case '/admin':
         echo 'admin home';
