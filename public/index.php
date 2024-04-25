69538462c875947411347b79e5bd87bf13f73bd0
Luis Silva <luis.silva1mon@gmail.com>
2024-04-25 10:45:16 -0600

    echos for testing

--- public/index.php
+++ public/index.php
@@ -6,9 +6,9 @@ define('PROJECT_ROOT', __DIR__ . '/../');
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if (strpos($requestUri, '/api/') === 0) {
+if (strpos($requestUri, '/api') === 0) {
     require_once PROJECT_ROOT . 'src/routes/apiRoutes.php';
-} elseif (strpos($requestUri, '/admin/') === 0) {
+} elseif (strpos($requestUri, '/admin') === 0) {
     require_once PROJECT_ROOT . 'src/routes/adminRoutes.php';
 } else {
     require_once PROJECT_ROOT . 'src/routes/webRoutes.php';
--- src/routes/adminRoutes.php
+++ src/routes/adminRoutes.php
@@ -2,16 +2,21 @@
 
 switch ($_SERVER['REQUEST_URI']) {
     case '/admin':
+        echo 'admin home';
         $controller = new \App\Controller\LoginController();
         $controller->index();
         break;
     case '/admin/users':
+        echo 'admin users';
         break;
     case '/admin/movements':
+        echo 'admin movements';
         break;
     case '/admin/movement-categories':
+        echo 'admin movement-categories';
         break;
     case '/admin/accounts':
+        echo 'admin accounts';
         break;
     default:
         http_response_code(404);
