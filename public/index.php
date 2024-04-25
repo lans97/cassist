703004518e5baff2e204888939517411931bc39f
Luis Silva <luis.silva1mon@gmail.com>
2024-04-25 10:43:02 -0600

    includes fix

--- public/index.php
+++ public/index.php
@@ -2,21 +2,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-echo "entrypoint<br>";
-
 define('PROJECT_ROOT', __DIR__ . '/../');
-
-
-require_once PROJECT_ROOT . 'src/routes/webRoutes.php';
-require_once PROJECT_ROOT . 'src/routes/adminRoutes.php';
-require_once PROJECT_ROOT . 'src/routes/apiRoutes.php';
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if (strpos($requestUri, '/api/') === 0) {
-    include PROJECT_ROOT . 'src/routes/apiRoutes.php';
+    require_once PROJECT_ROOT . 'src/routes/apiRoutes.php';
 } elseif (strpos($requestUri, '/admin/') === 0) {
-    include PROJECT_ROOT . 'src/routes/adminRoutes.php';
+    require_once PROJECT_ROOT . 'src/routes/adminRoutes.php';
 } else {
-    include PROJECT_ROOT . 'src/routes/webRoutes.php';
+    require_once PROJECT_ROOT . 'src/routes/webRoutes.php';
 }
