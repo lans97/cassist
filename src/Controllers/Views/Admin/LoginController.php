c320ac842fcdb148bb4ca41d7de9f568b7abadb6
Luis Silva <luis.silva1mon@gmail.com>
2024-04-25 20:54:00 -0600

    Update LoginController.php

--- src/Controllers/Views/Admin/LoginController.php
+++ src/Controllers/Views/Admin/LoginController.php
@@ -4,11 +4,36 @@ class LoginController {
     public function index() {
         $title = "Login";
         $content = file_get_contents(PROJECT_ROOT . "views/admin/login.php");
-        include (PROJECT_ROOT . "templates/base.php");
+        include (PROJECT_ROOT . "templates/admin/base.php");
     }
-    public function handleLogin() {
+    
+    public function handlePost() {
+        require_once PROJECT_ROOT . "src/Utils/database.service.php";
+        $pdo = getPDOConnection();
+        $handler = new \App\API\Handlers\UsersHandler($pdo);
 
+        $login = $handler->login($_POST["username"], $_POST["password"]);
+        if ($login) {
+            $_SESSION["user-id"] = $login['id'];
+            header("Location: /admin/cruds");
+            exit();
+        } else {
+            echo "<script>alert('No login')</script>";
+            header("Location: /admin/login");
+            exit();
+        }
     }
     public function handleCalls() {
+        if (isset($_SESSION['user-id'])){
+            header("Location: /admin/cruds");
+            exit();
+        }
+        switch ($_SERVER['REQUEST_METHOD']){
+            case 'GET':
+                $this->index();
+                break;
+            case 'POST':
+                $this->handlePost();
+        }
     }
 }
