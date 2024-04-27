80866968f3c264c5f1cd067d2194c073b7745872
Luis Silva <luis.silva1mon@gmail.com>
2024-04-27 15:44:14 -0600

    Better form

--- views/register.php
+++ views/register.php
@@ -1,15 +1,15 @@
 <form action="" id="registerForm">
-    <label for="username">Username</label>
-    <input type="text" name="username" id="username">
-    <label for="mail">Mail</label>
-    <input type="text" name="mail" id="mail">
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password">
-    <label for="userPass2IN">Confirm Password</label>
-    <input type="password" id="userPass2IN">
-    <label for="super_user">Super User</label>
-    <input type="checkbox" name="super_user" id="super_user">
-    <button class="btn btn-primary" id="registerBTN">Add</button>
+    <label for="username">Username</label><br>
+    <input type="text" name="username" id="username"><br>
+    <label for="mail">Mail</label><br>
+    <input type="text" name="mail" id="mail"><br>
+    <label for="password">Password</label><br>
+    <input type="password" name="password" id="password"><br>
+    <label for="userPass2IN">Confirm Password</label><br>
+    <input type="password" id="userPass2IN"><br>
+    <label for="super_user">Super User</label><br>
+    <input type="checkbox" name="super_user" id="super_user"><br>
+    <button class="btn btn-primary" id="registerBTN">Add</button><br>
 </form>
 
 <script src="js/register.js"></script>
