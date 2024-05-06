1aa9944b176c7c0f24dd30184015c2175719f0f6
Luis Silva <luis.silva1mon@gmail.com>
2024-05-06 07:15:00 -0600

    Footer links and changes

--- templates/footer.php
+++ templates/footer.php
@@ -4,18 +4,9 @@
         <div class="row">
             <div class="col-md-6">
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-light">Home</a></li>
-                    <li><a href="#" class="text-light">About Us</a></li>
-                    <li><a href="#" class="text-light">Services</a></li>
-                    <li><a href="#" class="text-light">Contact</a></li>
-                </ul>
-            </div>
-            <div class="col-md-6 text-md-right">
-                <ul class="list-inline">
-                    <li class="list-inline-item"><a href="#" class="text-light"><i class="fab fa-facebook-f"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="text-light"><i class="fab fa-twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="text-light"><i class="fab fa-instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="text-light"><i class="fab fa-linkedin-in"></i></a></li>
+                    <li><a href="/" class="text-light">Home</a></li>
+                    <li><a href="/about" class="text-light">About Us</a></li>
+                    <li><a href="https://www.github.com/lans97" class="text-light">Contact</a></li>
                 </ul>
             </div>
         </div>
